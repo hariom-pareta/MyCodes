215f8abe61addafac340a90debc6865a8ce282e8
Hariom Pareta <ohmpareta1804@gmail.com>
2024-12-02 12:03:29 +0530

    Time: 0 ms (100%), Space: 41.1 MB (9.45%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public int hammingWeight(int n) {
+        int ans = 0;
+        while (n != 0) {
+            n -= (n & -n);
+            ++ans;
+        }
+        return ans;
+    }
+}
